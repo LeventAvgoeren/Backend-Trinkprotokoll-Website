5ae805495db7d38053508140a7a38c198202689e
Levent avgören <leventavgoren@gmail.com>
2023-12-04 17:38:24 +0100

    verifyPasswordAndCreateJWT impl

--- src/services/JWTService.ts
+++ src/services/JWTService.ts
@@ -1,8 +1,38 @@
 import { LoginResource } from "../Resources";
-
+import { Pfleger } from "../model/PflegerModel";
+import { login } from "./AuthenticationService";
+import { JwtPayload, sign, verify }
+    from "jsonwebtoken";
 export async function verifyPasswordAndCreateJWT(name: string, password: string): Promise<string | undefined> {
-    throw new Error("Function verifyPasswordAndCreateJWT not implemented yet")
+    //geheimnis aus .env holen und nach gucken ob es gesetzt wurde 
+    const secret = process.env.JWT_SECRET;
+    const time = process.env.JWT_TTL //300 sekunden
+    if (!secret) {
+        throw Error("secret nicht gesetzt")
+    }
+    //Gucken ob login klappt 
+    let loginfo = await login(name, password)
+    if (!loginfo) {
+        throw Error("login fehlgeschlagen")
+    }
+
+    //payload der standart felder definiert 
+    const payload: JwtPayload = {
+        sub: loginfo.id,
+        role: loginfo.role,
+    }
+    //erzeugung des JWT mit unterschrift 
+    const jwtString = sign(
+        payload,
+        secret,
+        {
+            expiresIn: time,
+            algorithm: `HS256`
+        });
+
+    return jwtString
 }
+
 
 export function verifyJWT(jwtString: string | undefined): LoginResource {
     throw new Error("Function verifyJWT not implemented yet")
